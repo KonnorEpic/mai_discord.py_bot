2a536d8d672f407d030662f1640ba2d82c8bdfd9
IxLe <41942304+KonnorEpic@users.noreply.github.com>
2018-11-01 22:45:58 +0800

    Update nsfw.py

--- modules/nsfw.py
+++ modules/nsfw.py
@@ -19,6 +19,34 @@ class NSFW:
       await ctx.send(embed=emb)
     else:
       await ctx.send("This channel is not nsfw!")
+      
+  @commands.command()
+  async def anal(self, ctx):
+    if ctx.channel.is_nsfw():
+      res = requests.get("https://nekobot.xyz/api/image?type=anal")
+      data = res.json()
+      
+      emb = discord.Embed(title="Anal successfully generate!")
+      emb.set_image(url=data["message"])
+      emb.set_footer(text="Requested by %s" % (ctx.author.name))
+      
+      await ctx.send(embed=emb)
+    else:
+      await ctx.send("This channel is not nsfw!")
+      
+  @commands.command()
+  async def pussy(self, ctx):
+    if ctx.channel.is_nsfw():
+      res = requests.get("https://nekobot.xyz/api/image?type=pussy")
+      data = res.json()
+      
+      emb = discord.Embed(title="Pussy successfully generate!")
+      emb.set_image(url=data["message"])
+      emb.set_footer(text="Requested by %s" % (ctx.author.name))
+      
+      await ctx.send(embed=emb)
+    else:
+      await ctx.send("This channel is not nsfw!")
 
 def setup(bot):
   bot.add_cog(NSFW(bot))
